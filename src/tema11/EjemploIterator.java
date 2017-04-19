93d9ba5e45c38a133e3b90ddb903089de25044d7
lidiperma <lidiperma@gmail.com>
2017-04-19 10:03:16 +0200

    Es para mi repositorio local

--- src/tema11/EjemploIterator.java
+++ src/tema11/EjemploIterator.java
@@ -9,21 +9,25 @@ public class EjemploIterator {
     
  public static void main (String[] Args) {
      
- ArrayList <String> listaDeNombres = new ArrayList <String> ();
- listaDeNombres.add("Juan Pérez Sánchez");
- listaDeNombres.add("José Alberto Reverón Montes");
- String cadenaBuscar = "Alberto";
- System.out.println ("La cadena que buscamos es " + cadenaBuscar);
- 
- Iterator <String> it = listaDeNombres.iterator(); //Creamos el objeto it de tipo EjemploIterator con String
- String tmpAnalizando;
- while ( it.hasNext() ) {   //Utilizamos el método hasNext de los objetos tipo EjemploIterator
- tmpAnalizando = it.next(); //Utilizamos el método next de los objetos tipo EjemploIterator
- System.out.println ("Analizando elemento... " + tmpAnalizando);
- 
- if (tmpAnalizando.contains(cadenaBuscar) ) {
- System.out.println ("Cadena encontrada!!!");
- } else { } //else vacío. No hay acciones a ejecutar.
- } //Cierre del while
+     ArrayList <String> listaDeNombres = new ArrayList <String> ();
+     listaDeNombres.add("Juan Pérez Sánchez");
+     listaDeNombres.add("José Alberto Reverón Montes");
+     String cadenaBuscar = "Alberto";
+     System.out.println ("La cadena que buscamos es " + cadenaBuscar);
+
+     Iterator <String> it = listaDeNombres.iterator(); //Creamos el objeto it de tipo EjemploIterator con String
+     String tmpAnalizando;
+     while ( it.hasNext() ) {   //Utilizamos el método hasNext de los objetos tipo EjemploIterator
+            tmpAnalizando = it.next(); //Utilizamos el método next de los objetos tipo EjemploIterator
+            System.out.println ("Analizando elemento... " + tmpAnalizando);
+
+            if (tmpAnalizando.contains(cadenaBuscar) ) {
+                System.out.println ("Cadena encontrada!!!");
+            } else { } //else vacío. No hay acciones a ejecutar.
+     } //Cierre del while
+
+     for (String listaDeNombre : listaDeNombres){
+         System.out.println ("Lista"+listaDeNombre);
+     } 
  } //Cierre del main
 } //Cierre de la clase
